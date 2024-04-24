7614fd1ff924f9f39c15bafeddf991b7874b0334
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-04-24 17:15:31 +0300

    make build

---
+++ index.8f2df2a6.js
@@ -0,0 +1,2 @@
+document.querySelector("#thumbs").addEventListener("click",function(e){e.preventDefault(),document.querySelector("#largeImg").src=e.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.8f2df2a6.js.map
